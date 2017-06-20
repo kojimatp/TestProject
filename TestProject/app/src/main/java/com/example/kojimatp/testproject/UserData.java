74c1877ebb79a7334251d25a68310736b90fae6d
aaa <vodp@systena.co.jp>
2017-06-20 17:43:31 +0900

    add UserData

---
+++ TestProject/app/src/main/java/com/example/kojimatp/testproject/UserData.java
@@ -0,0 +1,12 @@
+package com.example.kojimatp.testproject;
+
+/**
+ * Created by DucVTT on 2017/06/20.
+ */
+
+public class UserData {
+    private String userName;
+    public String getUserName(){
+        return "DUCVTT";
+    }
+}
